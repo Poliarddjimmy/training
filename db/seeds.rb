0cfd121a0412f9334924bae8ab027fba371f9b68
Poliard Djimmy <poliarddjimmy@gmail.com>
2021-06-12 20:34:35 -0400

    added more seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,10 +38,22 @@ chapters = courses.each do |course|
   )
 end
 
+Course.first.chapters.create!(
+    title: "This is a lesson",
+    description: "This is a description for this lesson",
+    objective: "This is a objective for this lesson"
+  )
+
 Chapter.all.each do |chapter|
   chapter.lessons.create!(
     title: "#{Faker::Lorem.word}",
     description: "#{Faker::Lorem.word}",
-    content: "#{Faker::Lorem.word}",
+    content: "#{Faker::Lorem.word}"
   )
 end
+
+Chapter.first.lessons.create!(
+    title: "This is a lesson",
+    description: "This is a description for this lesson",
+    content: "This is a content for this lesson"
+  )
