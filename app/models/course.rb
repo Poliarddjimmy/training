a7e630db0faeab7d00d303b2ccdad12104044f01
Poliard Djimmy <poliarddjimmy@gmail.com>
2021-06-14 12:17:22 -0400

    added lessons_count, users_count, subscribed to course model

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,9 @@
 class Course < ApplicationRecord
     belongs_to :category, class_name: "Category", foreign_key: "category_id"
     has_many :chapters, class_name: "Chapter", foreign_key: "course_id"
-    has_and_belongs_to_many :users, join_table: "course_users", foreign_key: "user_id"
+    has_many :lessons, through: :chapters
+    has_many :course_users, class_name: "CourseUser", foreign_key: "course_id"
+    has_many :users, through: :course_users
     has_one :requirement, class_name: "Requirement", foreign_key: "course_id"
 
     after_validation :set_slug, only: [:create, :update]
@@ -13,13 +15,25 @@ class Course < ApplicationRecord
 
     def as_json(options={})
         super(
+            :methods => [:users_count, :lessons_count],
             :include => {
                 :requirement => {:only => [:price, :content, :duration]},
                 :category => {:only => [:name, :slug]},
                 :chapters => {:only => [:title, :slug], :include => {:lessons => {:only => [:title, :slug]}}},
-                :users => {:only => [:name, :username]}
             } 
         )
+    end
+
+    def lessons_count
+        self.lessons.count
+    end
+
+    def users_count
+        self.users.count
+    end
+
+    def subscribed(id)
+        self.users.where(id: id).first
     end
 
     private
