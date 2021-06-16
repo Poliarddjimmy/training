dd4072dda7e0f6146a4d87eb88c47c634f74c1f9
Poliard Djimmy <poliarddjimmy@gmail.com>
2021-06-15 23:30:40 -0400

    remove validation for slug

--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -23,16 +23,6 @@ RSpec.describe Lesson, type: :model do
     lesson2 = build(:lesson, title: "The first lesson")
     expect(lesson2).to_not be_valid
   end
-
-  it "is not valid without a slug" do
-    lesson2 = build(:lesson, slug: nil)
-    expect(lesson2).to_not be_valid
-  end
-
-  it "has a unique slug" do
-    lesson2 = build(:lesson, slug: "the-first-lesson")
-    expect(lesson2).to_not be_valid
-  end
   
   it "has a without description" do
     lesson2 = build(:lesson, description: nil)
@@ -45,10 +35,9 @@ RSpec.describe Lesson, type: :model do
   end
 
   it "has:" do
-    expect(@lesson).to respond_to(:completed_by_me, :as_json).with(1).argument
+    expect(@lesson).to respond_to(:as_json).with(1).argument
     expect(@lesson).to respond_to(:next_lesson, :previous_lesson, :next_lesson_by_chapter,:previous_lesson_by_chapter)
 
-    expect(@lesson.completed_by_me(@user.id)).to eq(@lesson.users.where(id: @user.id).first)
     expect(@lesson.next_lesson).to eq(Lesson.where('chapter_id = ?and id > ?',  @lesson.chapter_id, @lesson.id).first)
     expect(@lesson.previous_lesson).to eq(Lesson.where('chapter_id = ?and id < ?',  @lesson.chapter_id, @lesson.id).first)
     expect(@lesson.next_lesson_by_chapter).to eq(@lesson.course.chapters.where("id > #{@lesson.chapter.id}").first )
