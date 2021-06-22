f280c158128176d32a32286994d39d22624a8f8e
Poliard Djimmy <poliarddjimmy@gmail.com>
2021-06-22 01:10:18 -0400

    Add new field to users table

--- db/schema.rb
+++ db/schema.rb
@@ -108,6 +108,7 @@ ActiveRecord::Schema.define(version: 2021_06_19_195133) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "username"
+    t.string "slug"
     t.string "email"
     t.string "password_digest"
     t.string "avatar"
